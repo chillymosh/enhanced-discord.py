672e07609102778f62697fe202cc2e255c33538a
chillymosh <chillymosh@gmail.com>
2021-08-28 23:13:44 +0100

    removed erroneous import

--- discord/ext/commands/bot.py
+++ discord/ext/commands/bot.py
@@ -22,7 +22,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from TwitchIO.twitchio.ext.commands import utils
 from __future__ import annotations
 
 
